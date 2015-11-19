9b5bddc99f26f380e3a81850084e3893a3a392af
JP-Ellis <coujellis@gmail.com>
2015-11-20 00:34:22 +1100

    Add a push-script, untrack pdf.
    The PDF can be generated, so need need to keep track of it in master.
    
    Add a push script to automate the process of update the pages.
    
    Signed-off-by: JP-Ellis <coujellis@gmail.com>

---
+++ push-pages.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+echo "START: pre-push hook"
+
+commit_hash=$(git rev-parse --short HEAD)
+echo "Cleaning LaTeX output."
+latexmk -c
+echo "Regenerating LaTeX output."
+latexmk tikz-feynman
+if [[ $? -neq 0 ]]; then
+    echo "LatexMk exited with code $?" >&2
+    exit $?
+fi
+echo "Copying new pdf."
+cp tikz-feynman.pdf pages/tikz-feynman.pdf
+cd pages
+echo "Committing pages."
+git add --all
+git commit -m "Update to ${commit_hash}."
+git push
